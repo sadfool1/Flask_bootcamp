4fad5287bfffe97ee095a8cf8cfc5db2ec7038dc
James <takw14@gmail.com>
2020-12-30 13:57:29 +0800

    file_management

--- TestWebEnvironment/main.py
+++ TestWebEnvironment/main.py
@@ -3,8 +3,14 @@ from flask import Flask
 app = Flask (__name__)
 
 @app.route('/') #the key routing is this decorator
-def index():
+def index(): #127.0.0.1:5000
     return '<h1> Hello Puppy!! </h1>'
+
+@app.route('/information') #127.0.0.1:5000/information
+def info():
+    return '<h1> Puppies are cute! </h1>'
+
+
 
 
 if __name__ == '__main__':
